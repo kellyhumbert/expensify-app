be0f9cfd2e981604db6108e4c0042f4fecd44064
kellyhumbert <kelly.humbert@gmail.com>
2021-11-17 12:15:00 -0800

    remove test data

--- src/app.js
+++ src/app.js
@@ -3,27 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-// const waterBill = store.dispatch(addExpense({ description: 'Water Bill', amount: 12000 }));
-// const gasBill = store.dispatch(addExpense({ description: 'Gas Bill', amount: 3400, note: 'Just a note' , createdAt: 1000}));
-// const rentBill = store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-
-// store.dispatch(setTextFilter('water'));
-// setTimeout(() => {
-//     store.dispatch(setTextFilter('bill'));
-// }, 3000);
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses);
-console.log(store.getState());
 
 const jsx = (
     <Provider store={store}>
